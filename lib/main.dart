d917dede0bba029db4bd215190ce2c59b7699dde
A S M RAYHAN CHOWDHURY <136378207+asmrayhan1@users.noreply.github.com>
2025-01-01 18:23:49 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
+      // changes by abid
+
+      // Again changes by abid
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
